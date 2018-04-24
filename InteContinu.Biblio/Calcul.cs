694ca2ce69101a8ddcf797052dc6a378101a3997
Fouque Nicolas <n.fouque@outlook.fr>
2018-04-24 10:14:12 +0200

    Nouvelle classe calcul

---
+++ InteContinu.Biblio/Calcul.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InteContinu.Biblio
+{
+    public static class Calcul
+    {
+        //Méthode avec erreur volontaire
+        public static int GetInt(string s)
+        {
+            return (int.Parse(s) +1);
+        }
+
+    }
+}
